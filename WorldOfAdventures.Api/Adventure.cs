7edbc1d5466bfb690de5e75f8d4d5f7daa292af0
Mary Novosad <marichka.novosad@gmail.com>
2021-11-05 21:59:37 +0200

    Create an empty ASP .NET Core 3.1 web application, add Docker setup and .gitignore file

---
+++ WorldOfAdventures.Api/Adventure.cs
@@ -0,0 +1,7 @@
+namespace WorldOfAdventures
+{
+    public class Adventure
+    {
+
+    }
+}
---
+++ WorldOfAdventures.Api/Controllers/AdventureController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WorldOfAdventures.Api.Controllers
+{
+    [ApiController]
+    [Route("Adventure")]
+    public class AdventureController : ControllerBase
+    {
+        private readonly ILogger<AdventureController> _logger;
+
+        public AdventureController(ILogger<AdventureController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Adventure Get()
+        {
+            return new Adventure();
+        }
+    }
+}
